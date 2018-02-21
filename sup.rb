d85e7c151e677e344b1061fb2b00c3772b35363e
ABaldwinHunter <abaldwinhunter@codeclimate.com>
2018-02-21 12:19:03 -0500

    Add complex method for testing

---
+++ sup.rb
@@ -0,0 +1,21 @@
+class Complex
+  def complex_method
+    if "sup"
+      if "sup"
+        if "sup"
+          if "sup"
+            if "sup"
+              if "sup"
+                if "sup"
+                  puts "nested"
+                else
+                  puts "sup"
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
